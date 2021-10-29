ada3fa2c7a0b321898908a95623e17e0164504d1
Ahmed Afridee <a.afridee47@gmail.com>
2021-10-29 16:57:38 +0600

    updated...

---
+++ lib/Screens/ScanQRcode/ScanQrcode.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class ScanQRcode extends StatefulWidget {
+  @override
+  _ScanQRcodeState createState() => _ScanQRcodeState();
+}
+
+class _ScanQRcodeState extends State<ScanQRcode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
